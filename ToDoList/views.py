dc49c70a8b16c109e42c4c1ca7e5c54bf8078382
SarveshJoshi25 <mynameissarveshjoshi@gmail.com>
2021-12-13 15:58:17 +0530

    fixed - Empty JSON Body Error

--- ToDoList/views.py
+++ ToDoList/views.py
@@ -37,20 +37,23 @@ def user_login(request):
 
     """
     if request.method == "POST":
-        try:
-            received_data = json.loads(request.body.decode("utf-8"))
-            username = received_data["user_id"]
-            password = received_data["password"]
+        if request.body:
             try:
-                user = User.objects.get(user_id=username)
-                if bcrypt.checkpw(password.encode("utf-8"), user.password.encode("utf-8")):
-                    return JsonResponse({"JWT_Token": jwt.encode({'user_id': username}, jwt_secret, algorithm='HS256')})
-                else:
-                    return JsonResponse({"error": "Wrong Password."})
-            except User.DoesNotExist:
-                return JsonResponse({"error": "User with given credentials not found."})
-        except KeyError:
-            return JsonResponse({"error": "Required Credentials not found."})
+                received_data = json.loads(request.body.decode("utf-8"))
+                username = received_data["user_id"]
+                password = received_data["password"]
+                try:
+                    user = User.objects.get(user_id=username)
+                    if bcrypt.checkpw(password.encode("utf-8"), user.password.encode("utf-8")):
+                        return JsonResponse({"JWT_Token": jwt.encode({'user_id': username}, jwt_secret, algorithm='HS256')})
+                    else:
+                        return JsonResponse({"error": "Wrong Password."})
+                except User.DoesNotExist:
+                    return JsonResponse({"error": "User with given credentials not found."})
+            except KeyError:
+                return JsonResponse({"error": "Required Credentials not found."})
+        else:
+            return JsonResponse({"error": "Data not found."})
     else:
         return JsonResponse({"error": "POST Data not found."})
 
@@ -75,43 +78,48 @@ def user_signup(request):
 
     """
     if request.method == "POST":
-        try:
-            received_data = json.loads(request.body.decode("utf-8"))
-            username = received_data["user_id"]
-            email_address = received_data["email_id"]
-            password = received_data["password"]
+        if request.body:
             try:
-                user = User.objects.get(user_id=username)
-                return JsonResponse({"error": "Invalid Username / Username already exists."})
-            except User.DoesNotExist:
+                received_data = json.loads(request.body.decode("utf-8"))
+                username = received_data["user_id"]
+                email_address = received_data["email_id"]
+                password = received_data["password"]
                 try:
-                    user = User.objects.get(email_address=email_address)
-                    return JsonResponse({"error": "Invalid Email / Email already registered."})
+                    user = User.objects.get(user_id=username)
+                    return JsonResponse({"error": "Invalid Username / Username already exists."})
                 except User.DoesNotExist:
-                    pass
-            try:
-                email_address = validate_email(email_address).email
-            except EmailNotValidError:
-                return JsonResponse({"error": "Invalid Email Address."})
-            if MIN_USERNAME_LENGTH > len(username) or len(username) > MAX_USERNAME_LENGTH:
-                return JsonResponse({"error": "Username's Length should be between {0} and {1}".format(
-                    MIN_USERNAME_LENGTH, MAX_USERNAME_LENGTH)})
-            if MIN_PASSWORD_LENGTH > len(password) or len(password) > MAX_PASSWORD_LENGTH:
-                return JsonResponse({"error": "Password's Length should be between {0} and {1}".format(
-                    MIN_PASSWORD_LENGTH, MAX_PASSWORD_LENGTH)})
+                    try:
+                        user = User.objects.get(email_address=email_address)
+                        return JsonResponse({"error": "Invalid Email / Email already registered."})
+                    except User.DoesNotExist:
+                        pass
+                try:
+                    email_address = validate_email(email_address).email
+                except EmailNotValidError:
+                    return JsonResponse({"error": "Invalid Email Address."})
+                if MIN_USERNAME_LENGTH > len(username) or len(username) > MAX_USERNAME_LENGTH:
+                    return JsonResponse({"error": "Username's Length should be between {0} and {1}".format(
+                        MIN_USERNAME_LENGTH, MAX_USERNAME_LENGTH)})
+                if MIN_PASSWORD_LENGTH > len(password) or len(password) > MAX_PASSWORD_LENGTH:
+                    return JsonResponse({"error": "Password's Length should be between {0} and {1}".format(
+                        MIN_PASSWORD_LENGTH, MAX_PASSWORD_LENGTH)})
 
-            jwt_token = jwt.encode({'user_id': username}, jwt_secret, algorithm='HS256')
-            save_this = User(user_uuid=uuid4(), user_id=username, password=hash_password(password),
-                             email_address=email_address)
-            save_this.save()
-            return JsonResponse({"JWT_Token": jwt_token})
-        except KeyError:
-            return JsonResponse({"error": "Required fields not found."})
+                jwt_token = jwt.encode({'user_id': username}, jwt_secret, algorithm='HS256')
+                save_this = User(user_uuid=uuid4(), user_id=username, password=hash_password(password),
+                                 email_address=email_address)
+                save_this.save()
+                return JsonResponse({"JWT_Token": jwt_token})
+            except KeyError:
+                return JsonResponse({"error": "Required fields not found."})
+        else:
+            return JsonResponse({"error": "Data not found."})
     else:
         return JsonResponse({"error": "POST Request was expected."})
 
 
+@csrf_exempt
 def create_new_task(request):
+
     return JsonResponse({"message": "You are on Create New Task Page (Login Mandatory)."})
 
 
