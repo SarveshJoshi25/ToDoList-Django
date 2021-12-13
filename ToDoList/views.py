19e87bf2b8f708f6e637b23cd15d5cc25a66fb36
SarveshJoshi25 <mynameissarveshjoshi@gmail.com>
2021-12-13 15:49:57 +0530

    User Login View working.

--- ToDoList/views.py
+++ ToDoList/views.py
@@ -7,6 +7,7 @@ import bcrypt
 from config import jwt_secret
 from django.views.decorators.csrf import csrf_exempt
 import jwt
+
 MAX_USERNAME_LENGTH = 30
 MAX_PASSWORD_LENGTH = 60
 MIN_USERNAME_LENGTH = 3
@@ -17,12 +18,62 @@ def hash_password(password):
     return bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt()).decode('utf-8')
 
 
+@csrf_exempt
 def user_login(request):
-    return JsonResponse({"message": "You are on User Login Page."})
+    """
+        Login URL for Users. Returns JWT-Token encoded for authenticated user. JWT-Token should be passed to
+        Login Required URLs to insure authenticity.
+
+        Sample Input:
+        {
+            "user_id":"_sarveshJoshi22",
+            "password": "SarveshJoshiPassword"
+        }
+
+        Sample Output:
+        {
+            "JWT_Token": "wow-this-is-a-jwt-token"
+        }
+
+    """
+    if request.method == "POST":
+        try:
+            received_data = json.loads(request.body.decode("utf-8"))
+            username = received_data["user_id"]
+            password = received_data["password"]
+            try:
+                user = User.objects.get(user_id=username)
+                if bcrypt.checkpw(password.encode("utf-8"), user.password.encode("utf-8")):
+                    return JsonResponse({"JWT_Token": jwt.encode({'user_id': username}, jwt_secret, algorithm='HS256')})
+                else:
+                    return JsonResponse({"error": "Wrong Password."})
+            except User.DoesNotExist:
+                return JsonResponse({"error": "User with given credentials not found."})
+        except KeyError:
+            return JsonResponse({"error": "Required Credentials not found."})
+    else:
+        return JsonResponse({"error": "POST Data not found."})
 
 
 @csrf_exempt
 def user_signup(request):
+    """
+        Signup URL for Users to create a new account. Returns JWT-Token for successful creation of new user account.
+        JWT-Token should be passed to every Login Required URLs to insure authenticity of the user.
+
+        Sample Input:
+        {
+            "user_id":"_sarveshJoshi22",
+            "password": "SarveshJoshiPassword",
+            "email_id":"valid@gmail.com"
+        }
+
+        Sample Output :
+        {
+            "JWT_Token": "wow-this-is-a-jwt-token"
+        }
+
+    """
     if request.method == "POST":
         try:
             received_data = json.loads(request.body.decode("utf-8"))
@@ -50,9 +101,9 @@ def user_signup(request):
                     MIN_PASSWORD_LENGTH, MAX_PASSWORD_LENGTH)})
 
             jwt_token = jwt.encode({'user_id': username}, jwt_secret, algorithm='HS256')
-            save_this = User(user_uuid=uuid4(), user_id=username, password=hash_password(password), email_address=email_address)
+            save_this = User(user_uuid=uuid4(), user_id=username, password=hash_password(password),
+                             email_address=email_address)
             save_this.save()
-
             return JsonResponse({"JWT_Token": jwt_token})
         except KeyError:
             return JsonResponse({"error": "Required fields not found."})
